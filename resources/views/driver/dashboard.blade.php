c8e3e781226c0f9a626fda55db3cf2dad6a1c019
Thushanthy <thushanthywalter10@gmail.com>
2020-01-31 23:10:24 +0530

    test

--- resources/views/driver/dashboard.blade.php
+++ resources/views/driver/dashboard.blade.php
@@ -1,2 +1,3 @@
 <h1>Driver worked</h1>
 <h2>ascsdc</h2>
+test Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, deleniti. Voluptas incidunt, nobis sequi quidem libero architecto saepe beatae facere dolorem quod voluptatibus recusandae expedita ducimus harum voluptatem nostrum perspiciatis.
